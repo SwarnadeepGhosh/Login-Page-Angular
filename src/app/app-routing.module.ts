f054cbd366b37bafb777ccb332d624cb188b2709
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2021-05-07 15:54:58 +0530

    Advance routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegComponent } from './reg/reg.component';
+import { DeptDetailComponent } from './test/dept-detail/dept-detail.component';
+import { DeptListComponent } from './test/dept-list/dept-list.component';
+import { EmpComponent } from './test/emp/emp.component';
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'test', component: TestComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'reg', component: RegComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'test/departments', component: DeptListComponent },
+  //added a route with placeholder id parameter, which will be change according to click
+  { path: 'test/departments/:id', component: DeptDetailComponent },
+  { path: 'test/employees', component: EmpComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
